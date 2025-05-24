24e8d73e815a2d746302b72274b02b8abbcd9172
Andre Miranda Bernarde <ambernarde@gmail.com>
2025-05-23 22:14:48 -0300

    End of section 11

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -7,4 +7,7 @@ public class UserDto
    public required string Username { get; set; }
 
    public required string Token { get; set; }
+
+    public string? PhotoUrl { get; set; }
+
 }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,10 @@
+import { Photo } from './../_models/photo';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-import { of, tap } from 'rxjs';
+import { of, tap, map } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -29,6 +31,34 @@ export class MembersService {
       tap(() => {
         this.members.update(members => members.map(m => m.username === member.username
           ? member : m))
+      })
+    )
+  }
+
+  setMainPhoto(photo: Photo){
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
+      tap( () => {
+        this.members.update(members =>  members.map( m => {
+            if(m.photos.includes(photo)) {
+              m.photoUrl = photo.url
+            }
+            return m;
+        }))
+      }
+
+      )
+    )
+  }
+
+  deletePhoto(photo : Photo){
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo).pipe(
+      tap(() => {
+         this.members.update(members => members.map(m => {
+               if(m.photos.includes(photo)){
+                  m.photos = m.photos.filter(x => x.id !== photo.id)
+               }
+               return m
+         }))
       })
     )
   }
